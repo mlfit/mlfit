38fe16b2dd75dc5e24569a03ddf489a555ce9762
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-11-28 12:48:10 +0100

    trivial: test title

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -1,6 +1,6 @@
 context("import")
 
-test_that("import minitoy example", {
+test_that("import minitoy and toy examples", {
   require(plyr)
   results <- llply(setNames(nm=c("minitoy", "toy")), import_IPAF_results)
   print(results)
