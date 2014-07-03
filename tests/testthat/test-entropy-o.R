cdd8a7a0aa7a8c92bad119fd51cc8ffdd69caaf9
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-07-03 16:28:19 +0200

    add failing example for entropy fitting

--- tests/testthat/test-entropy-o.R
+++ tests/testthat/test-entropy-o.R
@@ -6,7 +6,7 @@ context("entropy_o")
 test_that("entropy_o algorithm", {
   require(plyr)
   require(kimisc)
-  test_names <- c("minitoy", "toy", "dummytoy", "multitoy", "onetoy")
+  test_names <- c("minitoy", "toy", "dummytoy", "multitoy", "onetoy", "bitoy")
   test_paths <- system.file(file.path("extdata", test_names), package = "MultiLevelIPF")
   results <- llply(setNames(test_paths, nm=test_names), import_IPAF_results)
   llply(results, function(problem) { ml_fit_entropy_o(problem); expect_true(TRUE) })
