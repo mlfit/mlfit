3f3727a7850d85c452d9429979375850079d0852
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-13 05:22:05 +0200

    Bump version to 0.0-13

--- R/MultiLevelIPF-package.R
+++ R/MultiLevelIPF-package.R
@@ -8,8 +8,8 @@
 #' \tabular{ll}{
 #'   Package: \tab MultiLevelIPF\cr
 #'   Type: \tab Package\cr
-#'   Version: \tab 0.0-12\cr
-#'   Date: \tab 2014-11-21\cr
+#'   Version: \tab 0.0-13\cr
+#'   Date: \tab 2015-04-13\cr
 #'   License: \tab GPL v3\cr
 #' } ~~
 #' An overview of how to use the package, including the most
