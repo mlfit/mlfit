8ed33b47453a7322c80d55964fd3d46c7657480f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-26 21:10:29 +0200

    test for ind+group

--- tests/testthat/test-corner.R
+++ tests/testthat/test-corner.R
@@ -43,3 +43,22 @@ test_that("Only grand total for individual", {
   expect_equal(as.vector(flat$weights %*% flat$reverse_weights_transform), rep(1, 3))
   expect_equal(flat$weights, 3)
 })
+
+test_that("Grand total for individual and group", {
+  ref_sample <- data.frame(gid=1:3)
+  ref_sample <- plyr::adply(ref_sample, 1, function(chunk)
+    data.frame(iid = seq_len(chunk$gid), n = chunk$gid))
+
+  group_control = data.frame(N=3)
+  individual_control = data.frame(N=6)
+  problem <- fitting_problem(
+    ref_sample,
+    field_names = special_field_names("gid", "iid", "n", "N"),
+    individual_controls = list(individual_control),
+    group_controls = list(group_control)
+  )
+  flat <- flatten_ml_fit_problem(problem, verbose = TRUE)
+  expect_equal(nrow(flat$ref_sample), 2)
+  expect_equal(as.vector(flat$weights %*% flat$reverse_weights_transform), rep(1, 6))
+  expect_equal(flat$weights, rep(1, 3))
+})
