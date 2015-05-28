fa75abc7f52482493c69cc702fb4e8cf0001d636
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-28 16:18:38 +0200

    test for error

--- tests/testthat/test-flatten.R
+++ tests/testthat/test-flatten.R
@@ -55,8 +55,29 @@ test_that("auto controls", {
   )
 
   problem <- fitting_problem(ref_sample, controls, field_names)
-  flat <- flatten_ml_fit_problem(problem)
+  expect_message(flat <- flatten_ml_fit_problem(problem, verbose = TRUE),
+                 "as count column")
   expect_equal(flat$fitting_problem, problem)
   expect_equal(as.vector(group_id %*% flat$weights_transform %*% flat$reverse_weights_transform),
                group_id)
+})
+
+test_that("error with auto controls", {
+  group_id <- c(1, 2, 2, 3, 3, 3)
+  ref_sample <- data.frame(group_id=group_id, ind=letters[1:2], group=LETTERS[group_id])
+  controls <- list(
+    group = list(
+      data.frame(group = LETTERS[1:3])
+    ),
+    individual = list(
+      data.frame(ind = LETTERS[1:2])
+    )
+  )
+  field_names <- list(
+    groupId = "group_id"
+  )
+
+  problem <- fitting_problem(ref_sample, controls, field_names)
+  expect_error(flat <- flatten_ml_fit_problem(problem, verbose = TRUE),
+                 "among control columns")
 })
