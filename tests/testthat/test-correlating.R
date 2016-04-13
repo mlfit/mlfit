f36b0933f2b8bd5d8afd3edf8437dedba52d3896
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-04-13 13:15:27 +0200

    add test for correlating variables

---
+++ tests/testthat/test-correlating.R
@@ -0,0 +1,37 @@
+context("correlating")
+
+test_that("trivially correlated variables", {
+  N <- 1000
+  M <- 60
+  x <- runif(N) * M
+
+  d <- rep(runif(1), N)
+
+  rs <-
+    data.frame(x2 = cut(x, seq(0, M, by = 2), right = FALSE),
+               x5 = cut(x, seq(0, M, by = 5), right = FALSE),
+               x10 = cut(x, seq(0, M, by = 10), right = FALSE))
+  rs$id <- seq_along(rs$x2)
+
+  x2c <- data.frame(x2 = kimisc::ofactor(levels(rs$x2)))
+  x2c$N <- 2
+
+  x5c <- data.frame(x5 = kimisc::ofactor(factor(levels(rs$x5))))
+  x5c$N <- 5
+
+  x10c <- data.frame(x10 = kimisc::ofactor(factor(levels(rs$x10))))
+  x10c$N <- 10
+
+  problem <- fitting_problem(
+    rs,
+    field_names = special_field_names("id", "id", "", "N"),
+    individual_controls = list(x2c, x5c, x10c),
+    group_controls = list())
+
+  flat <- MultiLevelIPF::flatten_ml_fit_problem(
+    problem, model_matrix_type = "combined", verbose = TRUE)
+
+  fit <- ml_fit_dss(flat, method = "linear")
+
+  expect_equal(unname(fit$residuals), rep(0, length(fit$residuals)))
+})
