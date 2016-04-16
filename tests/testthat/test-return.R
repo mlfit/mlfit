a44fac71b4420e5b1a9bb96f8021f108eb7af6e4
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-04-16 23:55:23 +0200

    test return value for nonconvergence

--- tests/testthat/test-return.R
+++ tests/testthat/test-return.R
@@ -10,3 +10,18 @@ test_that("success = TRUE", {
     expect_true(fit$success, info = algo)
   }
 })
+
+test_that("success = FALSE", {
+  bad_problem <-
+    fitting_problem(
+      ref_sample = data_frame(gid = 1:2, iid = 1:2, g = 1, i = 1),
+      group_controls = list(data_frame(g = 1, n = 4)),
+      individual_controls = list(data_frame(i = 1, n = 3)),
+      field_names = special_field_names("gid", "iid", count = "n"))
+
+
+  for (algo in algos) {
+    fit <- ml_fit(algo, bad_problem)
+    expect_false(fit$success, info = algo)
+  }
+})
