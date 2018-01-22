bbb83f7c7d41ebea42b8d537d8e01058223fb122
Kirill Müller <krlmlr+r@mailbox.org>
2018-01-22 13:46:06 +0100

    add overview

--- R/MultiLevelIPF-package.R
+++ R/MultiLevelIPF-package.R
@@ -1,3 +1,15 @@
+#' @details
+#' To use this package, you need to:
+#'
+#' 1. Specify your fitting problem with [fitting_problem()]
+#' 1. Optionally, convert the fitting problem to a structure that can be
+#'     processed by the algorithms with [flatten_ml_fit_problem()]; this is
+#'     helpful if you want to run the same fitting problem with multiple
+#'     algorithms and compare results.
+#' 1. Compute weights with one of the algorithms provided in this package with
+#'     [ml_fit()] or one of the specialized functions
+#' 1. Analyze weights or residuals, e.g. with [compute_margins()]
+#'
 #' @import methods
 #' @import dplyr
 #' @importFrom Matrix Matrix sparseMatrix sparse.model.matrix
