060616df53f589a2e2955318c22402e8dc0372f4
asiripanich <17020181+asiripanich@users.noreply.github.com>
2021-04-17 17:20:23 +1000

    save toy example RDS files as RDS version 2

--- data-raw/make_toy_ml_problems.R
+++ data-raw/make_toy_ml_problems.R
@@ -1,21 +1,21 @@
 #' this script converts the toy examples as `fitting_problem` objects
-#' to `ml_problem` objects. 
+#' to `ml_problem` objects.
 
-toy_example_filedirs <- 
-    list.files(here::here("inst", "extdata"), full.names = TRUE)
+toy_example_filedirs <-
+  list.files(here::here("inst", "extdata"), full.names = TRUE)
 
 toy_fitting_problems <-
-    lapply(toy_example_filedirs, readRDS)
+  lapply(toy_example_filedirs, readRDS)
 
 make_ml_problem <- function(x) {
-    stopifnot(is(x, "fitting_problem"))
-    class(x) <- "ml_problem"
-    x
+  stopifnot(is(x, "fitting_problem"))
+  class(x) <- "ml_problem"
+  x
 }
 
 toy_ml_problems <-
-    lapply(toy_fitting_problems, make_ml_problem)
+  lapply(toy_fitting_problems, make_ml_problem)
 
 mapply(function(filedir, toy_example) {
-    saveRDS(toy_example, filedir)
+  saveRDS(toy_example, filedir, version = 2)
 }, toy_example_filedirs, toy_ml_problems)
