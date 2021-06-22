afb450f2a623b5749d0053fb479dbbeec465c59c
asiripanich <17020181+asiripanich@users.noreply.github.com>
2021-06-22 18:15:38 +1000

    Removed old tests

--- tests/testthat/test-ml-list.R
+++
@@ -1,8 +0,0 @@
-test_that("mlfit a ml_problem list", {
-  problem <- readRDS(toy_example("Tiny"))
-  problem_list <- list(problem, problem)
-  fits <- ml_fit(problem_list, algorithm = "ipu")
-  expect_true(is.list(fits))
-  expect_true(length(fits) == length(problem_list))
-  expect_true(all(sapply(fits, is.ml_fit)))
-})
