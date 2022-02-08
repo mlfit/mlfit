f0760052af438e3ab8a4f39e6dbc1024b564cf12
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-02-08 14:38:01 +1100

    `ml_evaluate()` now prioritises integerised weights over fitted weights.

--- R/ml_evaluate.R
+++ R/ml_evaluate.R
@@ -1,43 +1,58 @@
-#' Evaluate fitted weights
+#' Evaluate fitted or integerised weights
+#' 
+#' This function evaluates sum of squared errors, standardised RMSE, 
+#' G statistic, and R2 of the fitted weights of a `ml_fit` object. However,
+#' if integerised weights are available, meaning `ml_integerise()` has 
+#' been called on the `ml_fit` object, it will evaluate the integerised
+#' weights instead.
 #'
-#' @param x a fitted problem.
-#' @param y a synthetic population generated from the fitted problem in `x`.
-#' @param ... dots.
+#' @param ml_fit a fitted problem of class `ml_fit`.
 #'
-#' @return a tibble with statistics.
+#' @return a tibble containing various statistics of the weights.
 #' @export
-ml_evaluate <- function(x, y = NULL, ...) {
-    stopifnot(is_ml_fit(x))
-    if (!is.null(y)) stopifnot(is.data.frame(y))
+ml_evaluate <- function(ml_fit) {
+    stopifnot(is_ml_fit(ml_fit))
 
-    actual <- x$flat$target_values
-    predicted <- actual + x$residuals
+    if (!is.null(ml_fit$int_weights)) {
+        predicted_weight_col <- "int_weights"
+        actual <- compute_margins(ml_fit$flat$ml_problem, ml_fit$weights) %>%
+            margin_to_df() %>%
+            dplyr::pull(..count..)
+        predicted <- compute_margins(ml_fit$flat$ml_problem, ml_fit$int_weights) %>%
+            margin_to_df() %>%
+            dplyr::pull(..count..)
+    } else {
+        predicted_weight_col <- "weights"
+        actual <- ml_fit$flat_weighted_values
+        predicted <- actual + ml_fit$residuals
+    }
 
     tibble(
-        fitting_algorithm = get_ml_fit_algo(x),
-        success = x$success,
-        iterations = x$iterations,
-        tol = x$tol,
-        zone = x$flat$ml_problem$zone,
-        min_weight = min(x$weights),
-        max_weight =  max(x$weights), 
-        gstat = compute_gstat(actual, predicted)$stat,
+        fitting_algorithm = get_ml_fit_algo(ml_fit),
+        integerisation_algorithm = get_ml_integerise_algo(ml_fit),
+        success = ml_fit$success,
+        iterations = ml_fit$iterations,
+        tol = ml_fit$tol,
+        zone = ml_fit$flat$ml_problem$zone,
+        min_weight = min(ml_fit[[predicted_weight_col]]),
+        max_weight =  max(ml_fit[[predicted_weight_col]]), 
+        gstat = compute_gstat(actual, predicted),
         sse = compute_sse(actual, predicted),
         srmse = compute_srmse(actual, predicted),
         r2 = compute_r2(actual, predicted)
     )
-    
 }
 
 #' g statistics
 compute_gstat <- function(actual, predicted) {
-    entropy::Gstat(actual, predicted)
+    predicted_pdf = predicted / sum(predicted)
+    actual_pdf = actual / sum(actual)
+    LR = ifelse(predicted_pdf > 0, log(predicted_pdf/actual_pdf), 0)
+    KL = sum(predicted_pdf * LR)
+    2 * sum(actual) * KL
 }
 
 #' sum of squared errors
-#'
-#' @examples
-#' sse(1:5, 2:6)
 compute_sse <- function(actual, predicted) {
     sum((actual - predicted)^2)
 }
@@ -50,9 +65,9 @@ compute_rmse <- function(actual, predicted) {
 
 #' standardised RMSE
 #'
-#' @description
-#' The SRMSE tends to decrease for larger sampling fractions, but a larger sample may still result in a larger SRMSE than a “good” smaller sample.
-#'
+#' Note from Kirill's thesis:
+#' "The SRMSE tends to decrease for larger sampling fractions, 
+#' but a larger sample may still result in a larger SRMSE than a “good” smaller sample."
 compute_srmse <- function(actual, predicted) {
     N <- length(actual)
     rmse <- compute_rmse(actual, predicted)
